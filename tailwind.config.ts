0349995d577e8c120dc3561bd36f279b6cdd78a1
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2024-03-05 22:35:49 -0800

    shadui config, tailwind config, global css shadui styling, clerk install

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,15 @@
 import type { Config } from "tailwindcss"
+import colors from 'tailwindcss/colors'
+import { withUt } from "uploadthing/tw"
 
-const config = {
+const config = withUt({
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './node_modules/@tremor/**/*.{js,ts,jsx,tsx}', // Tremor module
 	],
   prefix: "",
   theme: {
@@ -19,6 +22,83 @@ const config = {
     },
     extend: {
       colors: {
+        tremor: {
+          brand: {
+            faint: colors.stone[50],
+            muted: colors.stone[200],
+            subtle: colors.stone[400],
+            DEFAULT: colors.stone[500],
+            emphasis: colors.stone[700],
+            inverted: colors.stone[100],
+          },
+          background: {
+            muted: colors.stone[50],
+            subtle: colors.stone[100],
+            DEFAULT: colors.stone[100],
+            emphasis: colors.stone[700],
+          },
+          border: {
+            DEFAULT: colors.stone[200],
+          },
+          ring: {
+            DEFAULT: colors.stone[200],
+          },
+          content: {
+            subtle: colors.stone[400],
+            DEFAULT: colors.stone[500],
+            emphasis: colors.stone[700],
+            strong: colors.stone[900],
+            inverted: colors.stone[100],
+          },
+        },
+        'dark-tremor': {
+          brand: {
+            faint: '#0B1229',
+            muted: colors.stone[950],
+            subtle: colors.stone[800],
+            DEFAULT: colors.stone[500],
+            emphasis: colors.stone[400],
+            inverted: colors.stone[950],
+          },
+          background: {
+            muted: '#131A2B',
+            subtle: colors.stone[800],
+            DEFAULT: colors.stone[900],
+            emphasis: colors.stone[300],
+          },
+          border: {
+            DEFAULT: colors.stone[700],
+          },
+          ring: {
+            DEFAULT: colors.stone[800],
+          },
+          content: {
+            subtle: colors.stone[600],
+            DEFAULT: colors.stone[500],
+            emphasis: colors.stone[200],
+            strong: colors.stone[50],
+            inverted: colors.stone[950],
+          },
+        },
+        boxShadow: {
+          // light
+          'tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+          'tremor-card':
+            '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+          'tremor-dropdown':
+            '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+          // dark
+          'dark-tremor-input': '0 1px 2px 0 rgb(0 0 0 / 0.05)',
+          'dark-tremor-card':
+            '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
+          'dark-tremor-dropdown':
+            '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+        },
+        borderRadius: {
+          'tremor-small': '0.375rem',
+          'tremor-default': '0.5rem',
+          'tremor-full': '9999px',
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -72,9 +152,38 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      safelist: [
+        {
+          pattern:
+            /^(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ['hover', 'ui-selected'],
+        },
+        {
+          pattern:
+            /^(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ['hover', 'ui-selected'],
+        },
+        {
+          pattern:
+            /^(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+          variants: ['hover', 'ui-selected'],
+        },
+        {
+          pattern:
+            /^(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+        {
+          pattern:
+            /^(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+        {
+          pattern:
+            /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
+        },
+      ],
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+}) satisfies Config
 
 export default config
